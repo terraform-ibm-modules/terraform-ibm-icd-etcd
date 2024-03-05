a4641d9dd11f4dbec5be59566ca6a95aa8804ed5
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2024-03-05 13:37:21 -0500

    fix: update reference from key-protect to kms

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -15,7 +15,7 @@ module "resource_group" {
 ##############################################################################
 
 module "key_protect_all_inclusive" {
-  source                    = "terraform-ibm-modules/key-protect-all-inclusive/ibm"
+  source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
   version                   = "4.8.2"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
