244af93762c4d1404f273a64be21cee0bab6dfe8
Steve Peggs <peggs@uk.ibm.com>
2024-08-05 22:32:49 +0100

    fix: workaround provider idempotent issue

--- main.tf
+++ main.tf
@@ -170,6 +170,7 @@ resource "ibm_database" "etcd_db" {
       version,
       key_protect_key,
       backup_encryption_key_crn,
+      connectionstrings, # https://github.com/IBM-Cloud/terraform-provider-ibm/issues/5546
     ]
   }
   timeouts {
