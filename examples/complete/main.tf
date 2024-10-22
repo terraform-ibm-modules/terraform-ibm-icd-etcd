303f0797ffb452f59f9855d14b8fe21b460ecc08
shemau <shemau@gmail.com>
2024-10-22 11:10:05 +0100

    fix(deps): bump KMS with code change

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -16,15 +16,14 @@ module "resource_group" {
 
 module "key_protect_all_inclusive" {
   source                    = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                   = "4.15.13"
+  version                   = "4.16.4"
   resource_group_id         = module.resource_group.resource_group_id
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
   resource_tags             = var.resource_tags
   keys = [
     {
-      key_ring_name         = "icd-etcd"
-      force_delete_key_ring = true
+      key_ring_name = "icd-etcd"
       keys = [
         {
           key_name     = "${var.prefix}-etcd"
