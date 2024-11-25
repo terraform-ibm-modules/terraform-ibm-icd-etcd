bbcf0fd704403cf62845f3d458450757fdcbf82e
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2024-11-25 13:18:36 +0000

    fix: if condition

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ resource "ibm_database" "etcd_db" {
 
   ## This block is for if host_flavor IS NOT set
   dynamic "group" {
-    for_each = local.host_flavor_set && var.backup_crn == null ? [] : [1]
+    for_each = !local.host_flavor_set && var.backup_crn == null ? [1] : []
     content {
       group_id = "member" # Only member type is allowed for IBM Cloud Databases
       memory {
